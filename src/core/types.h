563c25bc45cf200efef001fd01dc22d0dbb0a15f
5dishapatil <disha.k.patil5@gmail.com>
2026-09-26 21:58:06 +0530

    Add Symbol

--- src/core/types.h
+++ src/core/types.h
@@ -48,4 +48,42 @@ namespace stock_exchange::core{
         FILLED,
         CANCELED,
     };
+
+    using Symbol = std::array<char, 8>; // we are using std::array instead of std::string to avoid dynamic memory allocation. we will be using a fixed size array of 8 characters to store the symbol. if the symbol is less than 8 characters, we will pad it with null characters.
+
+    constexpr Symbol make_symbol(std::string_view text) noexcept{
+        Symbol symbol{};
+        const auto length = std::min(symbol.size(), text.size());
+        for(std::size_t index = 0; index < length; ++index){
+            symbol[index] = text[index];
+        }
+        return symbol;
+    }
+
+    constexpr std::string_view symbol_view(const Symbol &symbol) noexcept{
+        std::size_t length = 0;
+        while(length < symbol.size() && symbol[length] != '\0'){
+            ++length;
+        }
+        return std::string_view{symbol.data(), length};
+    }
+
+    struct SymbolLess{
+        constexpr bool operator()(const Symbol &lhs, const Symbol &rhs) const noexcept{
+            return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+        }
+    };
+
+    static_assert(std::is_trivially_copyable_v<Price>);
+    static_assert(std::is_trivially_copyable_v<Quantity>);
+    static_assert(std::is_trivially_copyable_v<OrderId>);
+    static_assert(std::is_trivially_copyable_v<SequenceNumber>);
+    static_assert(std::is_trivially_copyable_v<Timestamp>);
+    static_assert(std::is_trivially_copyable_v<ParticipantId>);
+    static_assert(std::is_trivially_copyable_v<MatchID>);
+    static_assert(std::is_trivially_copyable_v<Side>);
+    static_assert(std::is_trivially_copyable_v<OrderType>);
+    static_assert(std::is_trivially_copyable_v<OrderStatus>);
+    static_assert(std::is_trivially_copyable_v<Symbol>);
+
 }
